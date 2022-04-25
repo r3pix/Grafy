09f29e3bcbc2991f4f4804f14ba5698beec57724
Piotr <peterson2000@interia.pl>
2022-04-25 13:25:09 +0200

    integrity added

--- Assets/Scripts/SaveGraph.cs
+++ Assets/Scripts/SaveGraph.cs
@@ -24,21 +24,113 @@ public class SaveGraph : MonoBehaviour
         
     }
     
+
+    public void checkIntegrity()
+    {
+        
+        GameObject[] vertices = GameObject.FindGameObjectsWithTag("vertex");
+
+        var edgeCount = gm.edgeList.Count;
+        var edgeTab = gm.edgeList;
+        var incidenceTab = new int[vertices.ToList().Count, edgeCount];
+
+        for (int i = 0; i < edgeCount; i++)
+        {
+            incidenceTab[edgeTab[i].Vertex2 - 1, i] = int.Parse(edgeTab[i].TextDistance.ToString()); //poczatek krawedzi
+            incidenceTab[edgeTab[i].Vertex1 - 1, i] = -int.Parse(edgeTab[i].TextDistance.ToString()); // koniec krawedzi
+        }
+
+        var jaggedTab = incidenceTab.ToJagged();
+
+        bool[] visited = new bool[vertices.ToList().Count];
+        List<int> queue = new List<int>();
+
+        queue.Add(0);
+        visited[0] = true;
+       
+        while (queue.Count > 0)
+        {
+            var id = queue[0];
+            var incidentIds = GetIncidentIds(vertices.ToList().Count,edgeCount,id,jaggedTab);
+            foreach(var idToAdd in incidentIds)
+            {
+                if (visited[idToAdd] == false)
+                {
+                    queue.Add(idToAdd);
+                    visited[idToAdd] = true;
+                }
+             }
+            queue.Remove(id);
+            
+        }
+        
+        if (checkResult(visited))
+        {
+            Debug.Log("Spójny");
+        }
+        else
+        {
+            Debug.Log("Niespójny");
+        }
+
+
+    }
+
+    public bool checkResult(bool[] result)
+    {
+        foreach (var res in result)
+        {
+            if(res == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<int> GetIncidentIds(int verticesCount, int edgeCount, int currentId, int[][] jaggedTab)
+    {
+        var edgesToCheck = new List<int>();
+        var childVerticesIds = new List<int>();
+        
+        for(int i=0; i<edgeCount; i++) // sprawdzenie ktore krawedzie zaczynaja sie w danym wierzcho³ku
+        {
+            if(jaggedTab[currentId][i] > 0)
+            {
+                edgesToCheck.Add(i);
+            }
+        }
+        
+        foreach(var edge in edgesToCheck) //sprawdzenie konców krawedzi
+        {
+            for(int j=0; j<verticesCount; j++)
+            {
+                if (jaggedTab[j][edge] < 0)
+                {
+                    childVerticesIds.Add(j);
+                }
+            }
+        }
+        return childVerticesIds;
+
+    }
+
+
     public void saveGraph()
     {
         GameObject[] vertices = GameObject.FindGameObjectsWithTag("vertex");
 
-        var count = gm.edgeList.Count;
-        var tabb = gm.edgeList;
-        var tab = new int[vertices.ToList().Count, count];
+        var edgeCount = gm.edgeList.Count;
+        var edgeTab = gm.edgeList;
+        var incidenceTab = new int[vertices.ToList().Count, edgeCount];
 
-        for (int i=0; i < count; i++)
+        for (int i=0; i < edgeCount; i++)
         {
-            tab[tabb[i].Vertex2 - 1, i] = int.Parse(tabb[i].TextDistance.ToString());
-            tab[tabb[i].Vertex1 - 1, i] = - int.Parse(tabb[i].TextDistance.ToString());
+            incidenceTab[edgeTab[i].Vertex2 - 1, i] = int.Parse(edgeTab[i].TextDistance.ToString()); //poczatek krawedzi
+            incidenceTab[edgeTab[i].Vertex1 - 1, i] = - int.Parse(edgeTab[i].TextDistance.ToString()); // koniec krawedzi
         }
 
-        File.WriteAllLines(@"D:\Grafy\Grafy\MyFile.txt", tab.ToJagged().Select(line => String.Join(" ", line)));
+        File.WriteAllLines(@"D:\Grafy\Grafy\MyFile.txt", incidenceTab.ToJagged().Select(line => String.Join(" ", line)));
     }
 }
 
